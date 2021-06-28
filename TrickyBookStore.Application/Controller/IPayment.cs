9477f7dd531825a7e3e1d3d21b00606fbd61ab34
Quoc Lien Hiep <quoc.lien.hiep@preciofishbone.se>
2021-06-28 18:39:32 +0700

    feat: setup serviceProvider and logger service, add IPayment

---
+++ TrickyBookStore.Application/Controller/IPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickyBookStore.Application.Controller
+{
+    internal interface IPayment
+    {
+        long GetPaymentOnAMonthForUser(int year, int month, int customerId);
+    }
+}
---
+++ TrickyBookStore.Application/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace TrickyBookStore.Application
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Set up DI
+            var serviceProvider = (ServiceProvider)new ServiceCollection()
+                .AddLogging()
+                //Add services in between
+                .BuildServiceProvider();
+
+            //configure console logging
+            var loggerFactoryService = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactoryService = LoggerFactory.Create(builder => builder.AddConsole());
+
+            var logger = loggerFactoryService
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            //Do thing here
+        }
+    }
+}
