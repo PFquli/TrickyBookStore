2d3a5745a00667c5e92d31d6e5a4c7327bbf69af
Quoc Lien Hiep <quoc.lien.hiep@preciofishbone.se>
2021-06-29 17:17:02 +0700

    feat: create shared service

--- TrickyBookStore.Services/PurchaseTransactions/PurchaseTransactionService.cs
+++ TrickyBookStore.Services/PurchaseTransactions/PurchaseTransactionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TrickyBookStore.Models;
 using TrickyBookStore.Services.Books;
+using TrickyBookStore.Services.Shared;
 
 namespace TrickyBookStore.Services.PurchaseTransactions
 {
@@ -18,25 +19,10 @@ namespace TrickyBookStore.Services.PurchaseTransactions
             _purchaseTransactions = Store.PurchaseTransactions.Data.ToList();
         }
 
-        private bool CheckIsFromDate(DateTimeOffset targetDate, DateTimeOffset fromDate)
-        {
-            return DateTimeOffset.Compare(targetDate, fromDate) > -1;
-        }
-
-        private bool CheckIsToDate(DateTimeOffset targetDate, DateTimeOffset toDate)
-        {
-            return DateTimeOffset.Compare(targetDate, toDate) < 1;
-        }
-
-        private bool CheckIsRightCustomer(long targetId, long customerId)
-        {
-            return targetId == customerId;
-        }
-
         private bool CheckIfTransactionIncluded(PurchaseTransaction pcTrans, long customerId, DateTimeOffset fromDate, DateTimeOffset toDate)
         {
-            bool isRightCustomer = CheckIsRightCustomer(pcTrans.CustomerId, customerId);
-            bool isRightDate = CheckIsFromDate(pcTrans.CreatedDate, fromDate) && CheckIsToDate(pcTrans.CreatedDate, toDate);
+            bool isRightCustomer = SharedService.CheckIsRightCustomer(pcTrans.CustomerId, customerId);
+            bool isRightDate = SharedService.CheckIsFromDate(pcTrans.CreatedDate, fromDate) && SharedService.CheckIsToDate(pcTrans.CreatedDate, toDate);
             return isRightCustomer && isRightDate;
         }
 
---
+++ TrickyBookStore.Services/Shared/SharedService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickyBookStore.Services.Shared
+{
+    public static class SharedService
+    {
+        public static bool CheckIsFromDate(DateTimeOffset targetDate, DateTimeOffset fromDate)
+        {
+            return DateTimeOffset.Compare(targetDate, fromDate) > -1;
+        }
+
+        public static bool CheckIsToDate(DateTimeOffset targetDate, DateTimeOffset toDate)
+        {
+            return DateTimeOffset.Compare(targetDate, toDate) < 1;
+        }
+
+        public static bool CheckIsRightCustomer(long targetId, long customerId)
+        {
+            return targetId == customerId;
+        }
+    }
+}
