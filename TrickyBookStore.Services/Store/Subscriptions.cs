158225ae37bc39c30ac03ab7357bc2b1a6a44886
Quoc Lien Hiep <quoc.lien.hiep@preciofishbone.se>
2021-06-29 19:02:16 +0700

    feat: implement Subscriptions as described by requirements

--- TrickyBookStore.Services/Store/Subscriptions.cs
+++ TrickyBookStore.Services/Store/Subscriptions.cs
@@ -5,42 +5,64 @@ namespace TrickyBookStore.Services.Store
 {
     public static class Subscriptions
     {
+        private static readonly Dictionary<SubscriptionTypes, Dictionary<string, double>> PriceDetailsPerType = new Dictionary<SubscriptionTypes, Dictionary<string, double>>
+        {
+            {
+                SubscriptionTypes.Premium, new Dictionary<string, double>
+                {
+                    { "ReadPrice", 0 },
+                    { "DiscountPrice", 85 },
+                    { "MaximumDiscountedItems", 3 }
+                }
+            },
+            {
+                SubscriptionTypes.CategoryAddicted, new Dictionary<string, double>
+                {
+                    { "ReadPrice", 0 },
+                    { "DiscountPrice", 85 },
+                    { "MaximumDiscountedItems", 3 }
+                }
+            },
+            {
+                SubscriptionTypes.Paid, new Dictionary<string, double>
+                {
+                    { "ReadPrice", 5 },
+                    { "DiscountPrice", 95 },
+                    { "MaximumDiscountedItems", 3 }
+                }
+            },
+            {
+                SubscriptionTypes.Free, new Dictionary<string, double>
+                {
+                    { "ReadPrice", 90 },
+                    { "DiscountPrice", 100 },
+                    { "MaximumDiscountedItems", 0 }
+                }
+            }
+        };
+
         public static readonly IEnumerable<Subscription> Data = new List<Subscription>
         {
-            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                }
+            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = 3,
+                PriceDetails = PriceDetailsPerType[SubscriptionTypes.Paid]
             },
-            new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                }
+            new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = 4,
+                PriceDetails = PriceDetailsPerType[SubscriptionTypes.Free]
             },
-            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                }
+            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = 1,
+                PriceDetails = PriceDetailsPerType[SubscriptionTypes.Premium]
             },
-            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                }
+            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 2,
+                PriceDetails = PriceDetailsPerType[SubscriptionTypes.CategoryAddicted],
+                // changable for testing
+                BookCategoryId = 2
             },
-            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                    { "FixPrice", 75 },
-                    ....
-                },
+            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 2,
+                PriceDetails = PriceDetailsPerType[SubscriptionTypes.CategoryAddicted],
                 BookCategoryId = 1
             },
-            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
-                PriceDetails = new Dictionary<string, double>
-                {
-                    { "FixPrice", 75 },
-                    ....
-                },
+            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 2,
+                PriceDetails = PriceDetailsPerType[SubscriptionTypes.CategoryAddicted],
                 BookCategoryId = 3
             }
         };
